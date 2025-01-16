6d4860a0fafecb3e9ccb968e5b13b935f40bdeb4
OFC <1ogidifavour@gmail.com>
2025-01-16 23:39:29 +0100

    Basic webpack setup

---
+++ alx-react/0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from "jquery";
+
+// Use jQuery to add paragraphs to the body
+$(document).ready(() => {
+  $('body').append('<p>ALX Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - ALX</p>');
+});
